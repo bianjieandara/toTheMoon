7235d4876b3b4e146f2c3a5574976d9427f6b48b
bianjieandara <bianjieandara@gmail.com>
2018-01-26 14:40:56 -0430

    2

--- js/app.js
+++ js/app.js
@@ -1,36 +1,10 @@
 (function(){
 'use strict'
 
-angular.module("myapp", ['ui.router'])
-.config(RoutesConfig)
+angular.module("myapp", [])
 .controller('controller1', controller1)
 
-RoutesConfig.$inject = ['$stateProvider','$urlRouterProvider']
 
-function RoutesConfig($stateProvider, $urlRouterProvider){
-$urlRouterProvider.otherwise("/home");
-
-$stateProvider.state('home',{
-	url: "/home",
-	templateUrl: "./templates/home.html"
-})
-.state('history',{
-	url: "/history",
-	templateUrl: "./templates/history.html"
-})
-.state('gallery',{
-	url: "/gallery",
-	templateUrl: "./templates/gallery.html"
-})
-.state('game-play',{
-	url: "/game-play",
-	templateUrl: "./templates/game-play.html"
-})
-.state('downloads',{
-	url: "/downloads",
-	templateUrl: "./templates/downloads.html"
-})
-};
 
 function controller1() {
 	var ctrl1=this;
